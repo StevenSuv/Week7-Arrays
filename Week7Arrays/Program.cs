9db8f486a44446f9a0d4de30b6cecb9dc80766f1
Opilane TTHK <opilane@tthk.ee>
2021-10-13 10:47:09 +0300

    Arrays

---
+++ Week7Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Week7Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] food = { "sandwich with cheese", "yogurt", "bananana", "bacon", "egg", };
+
+            food[3] = "sausage";
+            food[1] = "kefir";
+
+            Console.WriteLine("Menu: ");
+
+            for(int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine(food[i]);
+            }
+
+            Console.WriteLine("Pick an item (enter a number from 1 to 5");
+            int userChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine($"You have chosen {food[userChoice]}");
+
+        }
+    }
+}
